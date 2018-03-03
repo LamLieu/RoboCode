b09050e5ba9edff8692c4fd99a69635fd30585c9
Lam Lieu <35049466+LamLieu@users.noreply.github.com>
2018-03-03 14:36:53 -0800

    Added temp onHitWall Code

--- PolyBot.java
+++ PolyBot.java
@@ -2,12 +2,13 @@ package IfAndOnlyIfs;
 
 import robocode.*;
 import robocode.util.Utils;
+
 import java.awt.Color;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
 /**
- * PolyBot - a robot by (your name here)
+ * PolyBot - a robot by Lam Lieu and Juan Vera
  */
 public class PolyBot extends AdvancedRobot {
     /**
@@ -26,9 +27,9 @@ public class PolyBot extends AdvancedRobot {
         // and the next line:
 
         setColors(Color.green, Color.yellow, Color.green); // body,gun,radar
-		
-		// When the robot turns, the gun doesn't turn the same way
-		setAdjustGunForRobotTurn(true);
+
+        // When the robot turns, the gun doesn't turn the same way
+        setAdjustGunForRobotTurn(true);
 
         // Robot main loop
         while (true) {
@@ -51,10 +52,10 @@ public class PolyBot extends AdvancedRobot {
             //Activates when there are 1 or 2 bots left
 
             else {
-			   movement();
-			   turnGunRight(360);
-			   movement();
-			   turnGunLeft(360);
+                movement();
+                turnGunLeft(360);
+                movement();
+                turnGunLeft(360);
             }
         }
     }
@@ -82,77 +83,30 @@ public class PolyBot extends AdvancedRobot {
      */
     public void onHitByBullet(HitByBulletEvent e) {
         // Replace the next line with any behavior you would like
-        back(10);
+        int count = 0;
+        if (count % 2 == 0)
+            ahead(50);
+        else
+            back(50);
+        count++;
     }
 
     /**
      * onHitWall: What to do when you hit a wall
      */
     public void onHitWall(HitWallEvent e) {
-
-        /* For when bot hits top right corner */
-        if (getY() == getBattleFieldHeight() && getX() == getBattleFieldWidth()) {
-            turnRight(90);
-            ahead(100);
-            if (getY() == getBattleFieldHeight() && getX() == getBattleFieldWidth())
-                back(100);
-        }
-
-        /* For when bot hits bottom right corner */
-        if (getY() == 0 && getX() == 0) {
-            turnRight(90);
-            ahead(100);
-            if (getY() == 0 && getX() == 0) 
-                back(100);
-
-        }
-
-        /* For when bot hits top left corner */
-        if (getY() == getBattleFieldHeight() && getX() == 0) {
-            turnRight(90);
-            ahead(100);
-            if (getY() == 0 && getX() == 0) 
-                back(100);
-        }
-
-        /* For when bot hits bottom left corner */
-        if (getY() == 0 && getX() == getBattleFieldWidth()) {
-            turnRight(90);
-            ahead(100);
-            if (getY() == 0 && getX() == 0)
-                back(100);
-        }
-
-        /* For when the bot hits the top or bottom wall */
-        if (getY() == getBattleFieldHeight()) {
-            turnLeft(90);
-            ahead(100);
-        }
-        else if (getY() == 0) {
-            turnLeft(90);
-            ahead(100);
-        }
-
-        /* For when the bot hits the left or right wall */
-        if (getX() == getBattleFieldWidth()) {
-            turnLeft(90);
-            ahead(100);
-        }
-        else if (getX() == 0) {
-            turnLeft(90);
-            ahead(100);
-        }
-		execute();
+        turnRight(90);
+        ahead(100);
     }
 
     // Some code from http://robowiki.net/wiki/GoTo
     private void movement() {
         double targetX, targetY;
-		
+
         // Calculates the closest vertex of the diamond
-		targetY = battleFieldHeightComparison(getY());
-		targetX = battleFieldWidthComparison(getX());
-		// Makes the coordinate positive
+        targetY = battleFieldHeightComparison(getY());
+        targetX = battleFieldWidthComparison(getX());
+        // Makes the coordinate positive
         targetX = (targetX < 0) ? -targetX : targetX;
         targetY = (targetY < 0) ? -targetY : targetY;
         // Calculates the angle to target location
@@ -171,12 +125,13 @@ public class PolyBot extends AdvancedRobot {
         /* This is a simple method of performing set front as back */
         double turnAngle = Math.atan(Math.tan(targetAngle));
         setTurnRightRadians(turnAngle);
-        if(targetAngle == turnAngle) {
+        if (targetAngle == turnAngle) {
             setAhead(distance);
-        } else {
+        }
+        else {
             setBack(distance);
         }
-		execute();
+        execute();
     }
 
     /* Calculates which target x coordinate the bot should go based on spawn location */
