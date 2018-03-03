7c91f5518d63be3da876866ff334b0e627ea7fe5
juanvera11127 <35055901+juanvera11127@users.noreply.github.com>
2018-03-02 22:29:06 -0800

    Added current robocode with both our changes
    yea

--- PolyBot.java
+++ PolyBot.java
@@ -7,68 +7,70 @@ import java.awt.Color;
 /**
  * PolyBot - a robot by (your name here)
  */
-public class PolyBot extends Robot
+public class PolyBot extends AdvancedRobot
 {
 	/**
 	 * run: PolyBot's default behavior
 	 */
+
+	public boolean beginningGame = true;
+	public boolean midGame = false;
+	public boolean endGame = false;
+	boolean check = false;
+	public int totalBots;
+	public double enemyBearing;
+	public double  enemyHeading;
+	
 	public void run() {
 		// Initialization of the robot should be put here
-		int totalBots = getOthers();
+		totalBots = getOthers();			
 		double height = getBattleFieldHeight();
 		double width = getBattleFieldWidth();
-		boolean endGame = false;
-		boolean beginningGame = true;
-		boolean midGame = false;
-		int frameCount = 0;
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
 
-		setColors(Color.green,Color.yellow,Color.green); // body,gun,radar
+		int frameCount = 0;
+
+		setColors(Color.green,Color.yellow,Color.green);
 		
-		// Robot main loop
 		while(true) {
-		//Activates if loop when there are less than 50% bots remaining
-			frameCount++;
-			if(frameCount%20 == 0) {
+			if(endGame) {
+				turnGunLeft(enemyBearing);
+				turnLeft(enemyBearing);
+				setAhead(10000);
+			}
+			else {
+		   		movement();
+			   	turnGunRight(360);
+			   	movement();
+				turnGunLeft(360);
+			}				
+
+			if(check) {
+			check = false;
 				if(getOthers() < totalBots/2) {	
 					midGame = true;
 					beginningGame = false;
-				}
+			}
 				if(getOthers() == 2) {
 					endGame = true;
 					midGame = false;
 				}
 			}
-
-
-		//Activates when there are 1 or 2 bots left
-
-		else {
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
-			}
 		}
 	}
 	
-	public void shoot() {
-		
-	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
+		enemyBearing = e.getBearing();
+		enemyHeading = e.getHeading();
 		double distance = e.getDistance();
-		out.println(distance + "away from " + e.getName());
+		out.println(distance + " away from " + e.getName());
 		
-		double firePower = 500 / distance;
+		double firePower = 700 / distance;
 		out.println("firepower is at: " + firePower);
 		fire(firePower);
-		fire(firePower);
+		check = true;
+
 	}
 
 	/**
@@ -82,8 +84,116 @@ public class PolyBot extends Robot
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+    public void onHitWall(HitWallEvent e) {
+
+        /* For when bot hits top right corner */
+        if (getY() == getBattleFieldHeight() && getX() == getBattleFieldWidth()) {
+            turnRight(90);
+            ahead(100);
+            if (getY() == getBattleFieldHeight() && getX() == getBattleFieldWidth())
+                back(100);
+        }
+
+        /* For when bot hits bottom right corner */
+        if (getY() == 0 && getX() == 0) {
+            turnRight(90);
+            ahead(100);
+            if (getY() == 0 && getX() == 0) 
+                back(100);
+
+        }
+
+        /* For when bot hits top left corner */
+        if (getY() == getBattleFieldHeight() && getX() == 0) {
+            turnRight(90);
+            ahead(100);
+            if (getY() == 0 && getX() == 0) 
+                back(100);
+        }
+
+        /* For when bot hits bottom left corner */
+        if (getY() == 0 && getX() == getBattleFieldWidth()) {
+            turnRight(90);
+            ahead(100);
+            if (getY() == 0 && getX() == 0)
+                back(100);
+        }
+
+        /* For when the bot hits the top or bottom wall */
+        if (getY() == getBattleFieldHeight()) {
+            turnLeft(90);
+            ahead(100);
+        }
+        else if (getY() == 0) {
+            turnLeft(90);
+            ahead(100);
+        }
+
+        /* For when the bot hits the left or right wall */
+        if (getX() == getBattleFieldWidth()) {
+            turnLeft(90);
+            ahead(100);
+        }
+        else if (getX() == 0) {
+            turnLeft(90);
+            ahead(100);
+        }
+		execute();
+    }
+
+    // Some code from http://robowiki.net/wiki/GoTo
+    private void movement() {
+        double targetX, targetY;
+		
+        // Calculates the closest vertex of the diamond
+		targetY = battleFieldHeightComparison(getY());
+		targetX = battleFieldWidthComparison(getX());
+		// Makes the coordinate positive
+        targetX = (targetX < 0) ? -targetX : targetX;
+        targetY = (targetY < 0) ? -targetY : targetY;
+        // Calculates the angle to target location
+        double angleToTarget = Math.atan2(targetX, targetY);
+
+        /* Calculate the turn required get there */
+        double targetAngle = robocode.util.Utils.normalRelativeAngle(angleToTarget - getHeadingRadians());
+
+        /*
+         * The Java Hypot method is a quick way of getting the length
+         * of a vector. Which in this case is also the distance between
+         * our robot and the target location.
+         */
+        double distance = Math.hypot(targetX, targetY);
+
+        /* This is a simple method of performing set front as back */
+        double turnAngle = Math.atan(Math.tan(targetAngle));
+        setTurnRightRadians(turnAngle);
+        if(targetAngle == turnAngle) {
+            setAhead(distance);
+        } else {
+            setBack(distance);
+        }
+		execute();
+    }
+
+    /* Calculates which target x coordinate the bot should go based on spawn location */
+    private double battleFieldHeightComparison(double robotY) {
+        if (robotY < getBattleFieldHeight() / 2) {
+            return getBattleFieldHeight() / 10;
+        }
+        else if (robotY > getBattleFieldHeight() / 2) {
+            return getBattleFieldHeight() - getBattleFieldHeight() / 10;
+        }
+        return getBattleFieldHeight() / 10;
+    }
+
+    private double battleFieldWidthComparison(double robotX) {
+        if (robotX < getBattleFieldWidth() / 2) {
+            return getBattleFieldWidth() / 10;
+        }
+        else if (robotX > getBattleFieldWidth() / 2) {
+            return getBattleFieldWidth() - getBattleFieldWidth() / 10;
+        }
+        return getBattleFieldWidth() / 10;
+}
+	
 }
